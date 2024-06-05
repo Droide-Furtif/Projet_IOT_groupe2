66d2b9be480ceac02bb91a86b3711f126016ba2e
Merixouille <lino.remy@gmail.com>
2024-06-05 21:48:10 +0200

    Adding the secret key of the sensor_station

--- backend/src/db.py
+++ backend/src/db.py
@@ -26,9 +26,10 @@ class Database:
 		if self.sensor_station is not None:
 			creation_date = date.today().strftime("%d-%m-%Y")
 			sensor_station_data = {
-				"mac_id": mac_id,
-				"city": city,
-				"creation_date": creation_date
+				"Mac_id": mac_id,
+				"Secret_Key": secret_key,
+				"City": city,
+				"Creation_date": creation_date
 			}
 			try:
 				result = self.sensor_station.insert_one(sensor_station_data)
@@ -41,24 +42,33 @@ class Database:
 			print("La collection 'Sensor_Station' n'est pas disponible")
 			return 0
 
-	def adding_statement(self, temperature, humidity, pressure):
-		if self.statement is not None:
-			date = date.today().strftime("%d-%m-%Y")
-			time = date.time()strftime("%H:%M:%S")
-			statement_data = {
-				"temperature": temperature,
-				"humidity": humidity,
-				"pressure": pressure,
-				"date": date,
-				"time"
-			}
-			try:
-				result = self.statement.insert_one(statement_data)
-				print(f"Données transmises avec succès.\r ID : {result.inserted_id}")
-				return 1
-			except errors.PyMongoError as e:
-				print(f"Erreur lors de l'insertion des données : {e}")
-				return 0
+	def adding_statement(self, secret_key, temperature, humidity, pressure):
+		if self.sensor_station is not None :
+			query = {"Secret_Key": secret_key}
+			sensor_station = self.sensor_station.find(query)
+			if sensor_station.count() <> 0:
+				id_sensor_station = sensor_station.get("_id")
+				if self.statement is not None:
+					date = date.today().strftime("%d-%m-%Y")
+					time = date.today()strftime("%H:%M:%S")
+					statement_data = {
+						"Id_Sensor_Station_F": id_sensor_station,
+						"Temperature": temperature,
+						"Humidity": humidity,
+						"Pressure": pressure,
+						"Date": date,
+						"Time": time
+					}
+					try:
+						result = self.statement.insert_one(statement_data)
+						print(f"Données transmises avec succès.\r ID : {result.inserted_id}")
+						return 1
+					except errors.PyMongoError as e:
+						print(f"Erreur lors de l'insertion des données : {e}")
+						return 0
+				else:
+					print("La collection 'Statement' n'est pas disponible")
+					return 0
 		else:
-			print("La collection 'Statement' n'est pas disponible")
+			print("La collection 'Sensor_Station' n'est pas disponible")
 			return 0
